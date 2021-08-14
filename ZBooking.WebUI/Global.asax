4672f1bebd26bfb594cc2d06f81cf2a75e42f3b1
Ahmed Abdelaziz <a.abdelaziz.eg@gmail.com>
2021-08-14 17:40:43 +0300

    Add project files.

---
+++ ZBooking.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ZBooking.WebUI.MvcApplication" Language="C#" %>
---
+++ ZBooking.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZBooking.WebUI.Startup))]
+namespace ZBooking.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
